0f838ae613d476d04f3b6c6801ca3603a664fe6f
liangmingliang <2902753024@qq.com>
2022-04-25 02:14:34 +0800

    finished file

--- strvec/extra-credit/file.c
+++ strvec/extra-credit/file.c
@@ -1,5 +1,6 @@
 #include "file.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* detect_encoding() - reads through the file indicated by fd
  * to determine its encoding
@@ -10,7 +11,69 @@
  */
 enum encoding_type detect_encoding(int fd) {
   // TODO: your implementation here!
+  char buf[5];
+  size_t num = 0;
+  bool not_ascii = false;
+  bool is_empty = true;
+  while ((num = read(fd, buf, 1)) > 0) {
+    is_empty = false;
+    char a = 128;
+    if ((buf[0] & a) == 0) {
+      continue;
+    }
+    not_ascii = true;
+    char b = 192;
+    char c = 224;
+    char d = 240;
+    char e = 248;
+    if ((buf[0] & c) == b) {
+      num = read(fd, buf, 1);
+      if (num < 1) {
+        goto out;
+      }
+      if ((buf[0] & b) == a) {
+        continue;
+      } else {
+        goto out;
+      }
+    }
+    if ((buf[0] & d) == c) {
+      num = read(fd, buf, 2);
+      if (num < 2) {
+        goto out;
+      }
+      if (((buf[0] & b) == a) && ((buf[1] & b) == a)) {
+        continue;
+      } else {
+        goto out;
+      }
+    }
+    if ((buf[0] & e == d)) {
+      num = read(fd, buf, 3);
+      if (num < 3) {
+        goto out;
+      }
+      if (((buf[0] & b == a)) && ((buf[1] & b == a)) && ((buf[2] & b) == a)) {
+        continue;
+      } else {
+        goto out;
+      }
+    }
+  }
+  if (is_empty) {
+    goto out;
+  }
+  if (num < 0) {
+    goto out;
+  }
+  if (not_ascii) {
+    return UTF_8;
+  } else {
+    return ASCII;
+  }
+ out:
   return UNDEFINED;
+  
 }
 
 /* The current implementation of main() opens the file and calls
